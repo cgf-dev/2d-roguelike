108f9a7d396dbf5ca8ac09b300e7ac61dec2178a
cgf_dev <charliefrost50@googlemail.com>
2021-08-23 15:31:48 +0100

    random_room_gen
    Not working correctly atm

---
+++ 2D Roguelike/Assets/Scripts/Rooms/RoomSpawner.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    // Variables 
+    // 1 = need bottom door
+    // 2 = need top door
+    // 3 = need left door
+    // 4 = need right door
+    public int openingDirection;
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("SpawnRoom", 3f);
+    }
+
+    private void SpawnRoom()
+    {
+        if (spawned == false)
+        {
+            switch (openingDirection)
+            {
+                case 1:
+                    // Need to spawn a room with a BOTTOM door
+                    rand = Random.Range(0, templates.bottomRooms.Length);
+                    Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                    break;
+                case 2:
+                    // Need to spawn a room with a TOP door
+                    rand = Random.Range(0, templates.topRooms.Length);
+                    Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                    break;
+                case 3:
+                    // Need to spawn a room with a LEFT door
+                    rand = Random.Range(0, templates.leftRooms.Length);
+                    Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                    break;
+                case 4:
+                    // Need to spawn a room with a RIGHT door
+                    rand = Random.Range(0, templates.rightRooms.Length);
+                    Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                    break;
+            }
+            spawned = true;
+        } 
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
